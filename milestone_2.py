1c116ff95df6d3243340ca61c1b5ff47156ec00d
ashwi <ashu.tellis@gmail.com>
2023-02-02 18:24:41 +0000

    Creates a variable 'guess' and asks for user input - to enter a letter

--- milestone_2.py
+++ milestone_2.py
@@ -3,3 +3,6 @@ word_list = ['Apple', 'Plum', 'Grapes', 'Orange', 'Pineapple']
 print(word_list)
 word = random.choice(word_list)
 print(word)
+
+guess = input('Please enter a single letter here: ')
+print(guess)
