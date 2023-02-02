5db2115ae27467da622a7c4447aa25099b6b71e5
ashwi <ashu.tellis@gmail.com>
2023-02-02 18:43:16 +0000

    Add 'if statement' to check the variable 'guess' is of length 1 and an alphabet

--- milestone_2.py
+++ milestone_2.py
@@ -4,5 +4,9 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 
+
 guess = input('Please enter a single letter here: ')
-print(guess)
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
