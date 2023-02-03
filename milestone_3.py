0606db356b06482e6675ad42629f1ba68f4611ef
ashwi <ashu.tellis@gmail.com>
2023-02-03 14:16:23 +0000

    Adds list and generates random word. Adds 'if statement' to check if the guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,7 @@
+import random
+word_list = ['Apple', 'Plum', 'Grapes', 'Orange', 'Pineapple']
+word = random.choice(word_list)
+
 def check_guess():
     while True:
         try:
@@ -8,3 +12,7 @@ def check_guess():
                 raise Exception
         except:
             print("Invalid letter. Please, enter a single alphabetical character.")
+    if guess.lower() in word.lower():
+        print(f'Good guess! {guess} is in the word.')
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again.')
