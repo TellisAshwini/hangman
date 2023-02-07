45f69f68b60923f3b25ecfc993bf5979ddfacbb8
ashwi <ashu.tellis@gmail.com>
2023-02-07 22:18:06 +0000

    Updates the word_guessed attribute in Hangman class

--- milestone_4.py
+++ milestone_4.py
@@ -3,7 +3,7 @@ import random
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
-        self.word_guessed = []
+        self.word_guessed = ['_'] * len(self.word)
         self.num_letters = None
         self.num_lives = num_lives
         self.word_list = word_list
